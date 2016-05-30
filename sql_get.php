05a7ef7e894a5c3e23a2263f2de31ec66b1f95c6
David St.Amour <David St.Amour>
2016-05-29 20:07:06 -0400

    auto-message

--- sql_get.php
+++ sql_get.php
@@ -3,19 +3,21 @@ function open_connection() {
   if(strlen(getenv('OPENSHIFT_APP_NAME')) > 0){
     echo ' yes app name';
     $YOUR_DATABASE_NAME = getenv('OPENSHIFT_APP_NAME');
-    $host = getenv('OPENSHIFT_MYSQL_DB_HOST') . ":" . getenv('OPENSHIFT_MYSQL_DB_PORT');
+    $host = getenv('OPENSHIFT_MYSQL_DB_HOST');
+    $port = getenv('OPENSHIFT_MYSQL_DB_PORT');
     $user = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
     $password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
     echo ' ' . $host;
   } else {
     echo ' no app name';
     $YOUR_DATABASE_NAME = "php";
-    $host = "localhost:3306";
+    $host = "localhost";
+    $port = "3306";
     $user = "root";
     $password = "#r00tpass2";
   }
 
-  $con = new mysqli($host, $user, $password, $YOUR_DATABASE_NAME);
+  $con = new mysqli($host, $user, $password, $YOUR_DATABASE_NAME, $port);
   if($con->connect_error) {
     die("Could not connect: " . mysqli_error($con));
   } else {
