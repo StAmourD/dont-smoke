d0fbae75955e2a8c47d513f43d68050e8e210d30
David St.Amour <David St.Amour>
2016-05-29 20:08:02 -0400

    auto-message

--- sql_get.php
+++ sql_get.php
@@ -1,15 +1,12 @@
 <?php
 function open_connection() {
   if(strlen(getenv('OPENSHIFT_APP_NAME')) > 0){
-    echo ' yes app name';
     $YOUR_DATABASE_NAME = getenv('OPENSHIFT_APP_NAME');
     $host = getenv('OPENSHIFT_MYSQL_DB_HOST');
     $port = getenv('OPENSHIFT_MYSQL_DB_PORT');
     $user = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
     $password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-    echo ' ' . $host;
   } else {
-    echo ' no app name';
     $YOUR_DATABASE_NAME = "php";
     $host = "localhost";
     $port = "3306";
