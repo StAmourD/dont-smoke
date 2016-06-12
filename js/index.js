2fd1e8bc82c2a571d003664ba1e1642529db5735
David St.Amour <stamourd@hotmail.com>
2016-06-12 09:05:40 -0400

    removed vanilla dom value set in favor of jQuery method

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("#OK_btn").click(function(){
     $('#calendar').html('<span class="glyphicon glyphicon-refresh spinning"></span> Loading...');
-    var month = parseInt(document.getElementById('MonthSel').value, 10);
+    var month = parseInt($("#MonthSel").val(), 10);
     if (month < 1 || month > 12 || isNaN(month)) {
       alert('Month out of range: ' + month);
       $('#calendar').html('');
@@ -11,36 +11,33 @@ $(document).ready(function(){
   });
   $("#Next_btn").click(function(){
     $('#calendar').html('<span class="glyphicon glyphicon-refresh spinning"></span> Loading...');
-    var month = parseInt(document.getElementById('MonthSel').value, 10);
+    var month = parseInt($("#MonthSel").val(), 10);
     if (month >= 1 && month < 12) {
       month += 1;
     } else if (month == 12) {
       month = 1;
     }
-    document.getElementById('MonthSel').value = month;
-    $("#calendar").load('./calendar.php?month=' + month);
+    $("#Mon-" + month).trigger('click');
   });
   $("#Prev_btn").click(function(){
     $('#calendar').html('<span class="glyphicon glyphicon-refresh spinning"></span> Loading...');
-    var month = parseInt(document.getElementById('MonthSel').value, 10);
+    var month = parseInt($("#MonthSel").val(), 10);
     if (month > 1 && month <= 12) {
       month -= 1;
     } else if (month == 1) {
       month = 12;
     }
-    document.getElementById('MonthSel').value = month;
-    $("#calendar").load('./calendar.php?month=' + month);
+    $("#Mon-" + month).trigger('click');
   });
   $("#mod-add-one").click(function(){
     $('#calendar').html('<span class="glyphicon glyphicon-refresh spinning"></span> Loading...');
     var ClickedDay = $("#ModalText").data("current-date");
-    // var ClickedDayValue = $("#ModalText").data("current-value");
     // load php that will update DB
     $.ajax({
       url: "./update.php?date=" + ClickedDay
     }).done(function(data) {
       // refresh calendar
-      var Tmonth = document.getElementById('MonthSel').value;
+      var Tmonth = $("#MonthSel").val();
       $("#calendar").load('./calendar.php?month=' + Tmonth);
     });
   });
@@ -57,7 +54,7 @@ $(document).ready(function(){
       url: "./update.php?date=" + ClickedDay
     }).done(function(data) {
       // refresh calendar
-      var Tmonth = document.getElementById('MonthSel').value;
+      var Tmonth = $("#MonthSel").val();
       $("#calendar").load('./calendar.php?month=' + Tmonth);
     });
   });
@@ -72,7 +69,7 @@ $(document).ready(function(){
       url: "./update.php?date=" + ClickedDay + "&newcount=" + NewCount
     }).done(function(data) {
       // refresh calendar
-      var Tmonth = document.getElementById('MonthSel').value;
+      var Tmonth = $("#MonthSel").val();
       $("#calendar").load('./calendar.php?month=' + Tmonth);
       $('#myModal').modal('hide');
     });
@@ -89,16 +86,15 @@ $(document).ready(function(){
   })(jQuery);
 
   $(".MonthItem").click(function() {
-    // $("#MonthSel").value = $(this).prop('id');
-    document.getElementById('MonthSel').value = $(this).prop('id').substring(4,6);
+    $("#MonthSel").val($(this).prop('id').substring(4,6));
     $("#mod-ok").trigger("click");
-    $("#month-drop").text($(this).text());
+    $("#month-drop").html($(this).text() + '<span class="caret"></span>');
   });
 
 //  Main
   var d = new Date();
   var month = d.getMonth() + 1;
-  document.getElementById('MonthSel').value = month;
+  $("#MonthSel").val(month);
   $('#calendar').html('<span class="glyphicon glyphicon-refresh spinning"></span> Loading...');
   $("#calendar").load('./calendar.php?month=' + month);
   // jQuerry bind after div change
